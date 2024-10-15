ae580d4bc6bd0a797154365f9b06a0c8fb2c64cf
Zwylair <82669735+Zwylair@users.noreply.github.com>
2024-10-15 14:38:07 +0300

    added advancement when finished AltarGlass structure

--- src/main/kotlin/zwylair/pisskaland_overhaul/blocks/AltarGlass.kt
+++ src/main/kotlin/zwylair/pisskaland_overhaul/blocks/AltarGlass.kt
@@ -5,6 +5,7 @@ import net.minecraft.block.Blocks
 import net.minecraft.item.ItemGroup
 import net.minecraft.registry.RegistryKey
 import net.minecraft.server.world.ServerWorld
+import net.minecraft.util.Identifier
 import net.minecraft.util.math.BlockPos
 import net.minecraft.world.World
 import zwylair.pisskaland_overhaul.ModObject
@@ -36,8 +37,6 @@ class AltarGlass : ModObject.ModBlock(Settings.copy(Blocks.GLASS).nonOpaque()) {
 
     @Deprecated("Deprecated")
     override fun scheduledTick(state: BlockState, world: ServerWorld, pos: BlockPos, random: net.minecraft.util.math.random.Random) {
-        PSO.LOGGER.info("Custom block at {} is ticking!", pos)
-
         var layerCounter = 0
         for (layer in structureLayers) {
             layerCounter++
@@ -45,20 +44,21 @@ class AltarGlass : ModObject.ModBlock(Settings.copy(Blocks.GLASS).nonOpaque()) {
             var y = -(structureLayers.size - layerCounter)
             var z = -1
 
+            // check structure
             for (line in layer) {
                 var x = -1
 
                 for (block in line) {
                     if (block != world.getBlockState(pos.add(x, y, z)).block) {
-                        PSO.LOGGER.info(
-                            "(AltarGlass tick) Wrong block:\n" +
-                            "\tOn ${pos.add(x, y, z).toShortString()} (${world.getBlockState(BlockPos(x, y, z)).block.name.string})\n" +
-                            "\tEternal indexes:\n" +
-                            "\t\tlayer: ${structureLayers.indexOf(layer)}\n" +
-                            "\t\tline: ${layer.indexOf(line)}\n" +
-                            "\t\tblock: ${line.indexOf(block)}\n" +
-                            "\t${block.name.string} expected"
-                        )
+//                        PSO.LOGGER.info(
+//                            "(AltarGlass tick) Wrong block:\n" +
+//                            "\tOn ${pos.add(x, y, z).toShortString()} (${world.getBlockState(BlockPos(x, y, z)).block.name.string})\n" +
+//                            "\tEternal indexes:\n" +
+//                            "\t\tlayer: ${structureLayers.indexOf(layer)}\n" +
+//                            "\t\tline: ${layer.indexOf(line)}\n" +
+//                            "\t\tblock: ${line.indexOf(block)}\n" +
+//                            "\t${block.name.string} expected"
+//                        )
                         world.setBlockState(pos.add(x, y, z), Blocks.DIAMOND_BLOCK.defaultState)
                         world.scheduleBlockTick(pos, this, 20)
                         return
@@ -69,9 +69,22 @@ class AltarGlass : ModObject.ModBlock(Settings.copy(Blocks.GLASS).nonOpaque()) {
             }
         }
 
-        PSO.LOGGER.info("Structure checked successfully!")
+        val players = world.getPlayers { player ->
+            player.squaredDistanceTo(pos.x.toDouble(), pos.y.toDouble(), pos.z.toDouble()) < 100
+        }
 
-        world.scheduleBlockTick(pos, this, 20)
+        if (players.isNotEmpty()) {
+            val player = players[0]
+            val advancement = world.server.advancementLoader.get(Identifier("${PSO.MODID}/make_altar"))
+
+            if (advancement != null) {
+                val progress = player.advancementTracker.getProgress(advancement)
+
+                for (criterion in progress.unobtainedCriteria) {
+                    player.advancementTracker.grantCriterion(advancement, criterion)
+                }
+            }
+        } else { world.scheduleBlockTick(pos, this, 20) }
     }
 
     @Deprecated("Deprecated")
